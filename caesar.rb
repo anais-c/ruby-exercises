893358d578288410788247ffa79fe278ccd9e31c
anais-c <anaiskat@gmail.com>
2015-10-05 09:13:26 +0200

    Update caesar.rb

--- caesar.rb
+++ caesar.rb
@@ -1,29 +1,59 @@
+
+# First Iteration
 def solve_cipher(input)
-  letters = input.split("")
   
+  letters = input.split('')
   numbers = []
-
+  
   letters.each do |letter|
-
     if letter == "a"
-      number = "z".ord
+      numbers << "z".ord
     else
-      number = letter.ord - 1
+      numbers << letter.ord - 1
     end
-
-    numbers << number
   end
 
   unencrypted = []
 
   numbers.each do |num|
-
-    unencryp = num.chr
-    unencrypted << unencryp
+    unencrypted << num.chr
   end
-  
-  unencrypted.join
 
+  unencrypted
 end
 
 puts solve_cipher("ifmmpa")
+# should return "helloz"
+
+puts "_______________________________"
+
+# Second Iteration
+def solve_cipher(input)
+  
+  characters = input.scan(/./)
+  numbers = []
+  
+  characters.each do |letter|
+    if ("b".."z").include?(letter)
+      numbers << letter.ord - 1
+    elsif letter == "a"
+      numbers << "z".ord
+    else
+      numbers << letter  
+    end  
+  end  
+
+  unencrypted = []
+
+  numbers.each do |num| 
+    if (97..122).include?(num)
+      unencrypted << num.chr
+    else
+      unencrypted << num
+    end
+  end  
+
+  unencrypted
+end
+
+puts solve_cipher("ifmmpa. gmpxfs,")
